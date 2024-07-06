eae26dbe3eca26f9553d1a965f331ccf299d0a32
mike <lokilskdij@gmail.com>
2024-07-06 14:46:14 -0600

    feat:component list bitacora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ListAlimentosComponent} from "@/app/shared/components/list-alimentos/list-alimentos.component";
+import {ListBitacoraComponent} from "@/app/shared/components/list-bitacora/list-bitacora.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListAlimentosComponent],
+  imports: [RouterOutlet, ListAlimentosComponent, ListBitacoraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
