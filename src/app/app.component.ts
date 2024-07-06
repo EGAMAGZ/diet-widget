08065043ee28c16be2a957f19334ba6806c26fdd
[Cristina Garcia] <[gre.cristina20@gmail.com]>
2024-07-06 16:05:01 -0600

    feat:lista-alimentos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ListAlimentosComponent} from "@/app/shared/components/list-alimentos/list-alimentos.component";
+import {ListAlimentosBreakfast} from "@/app/shared/components/list-alimentos/list-alimentos.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListAlimentosComponent],
+  imports: [RouterOutlet, ListAlimentosBreakfast],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/components/list-alimentos/list-alimentos.component.ts
+++ src/app/shared/components/list-alimentos/list-alimentos.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
 
+
+import {ChangeDetectionStrategy, Component, computed, signal} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+export interface ListAlimentos {
+  name: string;
+  completed: boolean;
+  calorias?: number;
+  listalimentos?: ListAlimentos[];
+}
+
+/**
+ * @title Basic checkboxes
+ */
 @Component({
   selector: 'app-list-alimentos',
-  standalone: true,
-  imports: [],
   templateUrl: './list-alimentos.component.html',
-  styleUrl: './list-alimentos.component.scss'
+  styleUrl: './list-alimentos.component.scss',
+  standalone: true,
+  imports: [MatCheckboxModule, FormsModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ListAlimentosComponent {
+export class ListAlimentosBreakfast {
+  readonly comida = signal<ListAlimentos>({
+    name: 'Breakfast',
+    completed: false,
+    listalimentos: [
+      {name: 'Proteina(Huevos)', completed: false, calorias:200},
+      {name: 'Verduras', completed: false, calorias:50},
+      {name: 'Fruta', completed: false, calorias:70},
+    ],
+  });
 
+  readonly partiallyComplete = computed(() => {
+    const task = this.comida();
+    if (!task.listalimentos) {
+      return false;
+    }
+    return task.listalimentos.some(t => t.completed) && !task.listalimentos.every(t => t.completed);
+  });
+
+  update(completed: boolean, index?: number) {
+    this.comida.update(c => {
+      if (index === undefined) {
+        c.completed = completed;
+        c.listalimentos?.forEach(t => (t.completed = completed));
+      } else {
+        c.listalimentos![index].completed = completed;
+        c.completed = c.listalimentos?.every(t => t.completed) ?? true;
+      }
+      return {...c};
+    });
+  }
 }
